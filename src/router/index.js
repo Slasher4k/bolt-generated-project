e3de35bf4c4c9ce3ddd2760090096c25eb2b96ff
Slasher4k <ahatchard4@gmail.com>
2025-04-20 11:29:13 +1000

    Initial commit from your app

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
     import Home from '../views/Home.vue';
     import Dashboard from '../views/Dashboard.vue';
+    import SignUp from '../views/SignUp.vue';
+    import Login from '../views/Login.vue';
 
     const routes = [
       { path: '/', name: 'Home', component: Home },
-      { path: '/dashboard', name: 'Dashboard', component: Dashboard }
+      { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+      { path: '/signup', name: 'SignUp', component: SignUp },
+      { path: '/login', name: 'Login', component: Login }
     ];
 
     const router = createRouter({
---
+++ src/views/SignUp.vue
@@ -0,0 +1,30 @@
+<template>
+      <div class="container mt-5">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <h2 class="mb-4">Sign Up</h2>
+            <form>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter your email">
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+              </div>
+              <button type="submit" class="btn btn-primary">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <script>
+    export default {
+      name: 'SignUp'
+    };
+    </script>
+
+    <style scoped>
+    /* Add styles for the sign-up page here */
+    </style>
